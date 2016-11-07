e571d88521dcde625e48f95740e1b0a3768a73b9
Peter Rhodes <rhodepeter@hotmail.co.uk>
2016-11-07 14:41:49 +0000

    Add error check

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var http = require('http');
+var fs = require('fs');
 
 var message = 'Node workshop!'
 
 function handler(request, response){
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(message);
-  response.end();
+  var method = request.method;
+  console.log(method);
+  var endpoint = request.url;
+  console.log(endpoint);
+  if (endpoint === '/'){
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    fs.readFile(__dirname + '/public/index.html', function(error, file){
+      if (error){
+        console.log(error)
+        return;
+      }
+
+      response.end(file);
+    });
+  }
 }
 
 var server = http.createServer(handler);
